599c25ce1d1115281aa77c8b6194e4379f988c27
Artem Makarenko <prestigemakar@gmail.com>
2022-06-21 17:35:23 +0300

    Initial Commit

---
+++ HomeWork_Lesson5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_Lesson5
+//
+//  Created by Артем Макаренко on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
